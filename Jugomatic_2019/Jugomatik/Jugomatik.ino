f9aa7fb5a4c913b959e42ecf167223a7b9201ae0
matias <mati2almas@gmail.com>
2019-09-19 16:26:57 -0300

    final offline

--- Jugomatic_2019/Jugomatik/Jugomatik.ino
+++ Jugomatic_2019/Jugomatik/Jugomatik.ino
@@ -1,19 +1,15 @@
-#include <NewPing.h>/*LIBRERIA SENSOR*/
-
 #include <Keypad.h>
 #include <LiquidCrystal.h>
 
-LiquidCrystal lcd(2, 3, 4, 5, 6, 7);
+LiquidCrystal lcd(22, 24, 37, 35, 33, 31);
 
-int relay1 =42, relay2 =43, relay3 =44, relay4 =45, Trig, duracion, Echo, distancia, vasovacio; /*Agregue Trig, duracion, Echo, distancia, vasovacio; PORQUE NO ESTABAN DECLARADAS*/
+int led1 =50, led2 =51, led3 =52, led4 =53, Trig, duracion, Echo, distancia, vasovacio; /*Agregue Trig, duracion, Echo, distancia, vasovacio; PORQUE NO ESTABAN DECLARADAS*/
 
-/*SENSOR
-#define TRIGGER_PIN  12
-#define ECHO_PIN     13
-#define MAX_DISTANCE 100
-
-/*Crear el objeto de la clase NewPing*/
-/*NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+/*SENSOR*/
+const int trigPin =45;
+const int echoPin =43;
+long duration;
+int distanceCm;
 /*SENSOR*/
 
 //Config del Teclado
@@ -27,92 +23,143 @@ char botones[filas][colum]={
   {'#','0','*','D'}
 };
 
-byte pinesFila[filas]={22,24,26,28};
-byte pinesColu[colum]={30,32,34,36};
+byte pinesFila[filas]={2,3,4,5};
+byte pinesColu[colum]={7,6,9,8};
 
 
 Keypad teclado=Keypad( makeKeymap(botones),pinesFila,pinesColu,filas,colum );
 
 void setup() {
+  
   Serial.begin(9600);
   lcd.begin(16, 2);
-  lcd.print("Jugomatik");
+  lcd.print("Jugomatik 2.0");
   delay(2000);
     
   lcd.clear();
-  pinMode(relay1, OUTPUT);
-  pinMode(relay2, OUTPUT);
-  pinMode(relay3, OUTPUT);
-  pinMode(relay4, OUTPUT);
-  digitalWrite(relay1, LOW);
-  digitalWrite(relay2, LOW);
-  digitalWrite(relay3, LOW);
-  digitalWrite(relay4, LOW);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, LOW);
+  digitalWrite(led3, LOW);
+  digitalWrite(led4, LOW);
+
 }
 
 
 void loop() {
- char key = teclado.getKey();
-  
- digitalWrite(Trig,LOW);
+char key = teclado.getKey();
+
+
+ digitalWrite(trigPin,LOW);
  delayMicroseconds(4);
- digitalWrite(Trig,HIGH);
+ digitalWrite(trigPin,HIGH);
  delayMicroseconds(10);
- digitalWrite(Trig,LOW);
+ digitalWrite(trigPin,LOW);
   
- duracion = pulseIn(Echo,HIGH);
- duracion = duracion/2;
- distancia = duracion/29;
-  
- delay(500);
- if (distancia > vasovacio){
+ duration = pulseIn(echoPin,HIGH);
+ distanceCm = duration*0.034/2;
+ 
+ if (distanceCm > 13){
   lcd.clear();
   lcd.setCursor(0,0);
   lcd.print("Coloque el vaso");
   lcd.setCursor(0,1);
   lcd.print("y elija un jugo");
+  delay(2000);
+  lcd.clear();
   }
 
+
+ if(distanceCm < 13 ) {
+
+if (distanceCm < 9) {
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Coloque un");
+  lcd.setCursor(0,1);
+  lcd.print("vaso vacio");
+  delay(2000);
+  lcd.clear();  
+  }
  
- if(distancia <= vasovacio ) { 
+    
+    lcd.setCursor(0,0);
+    lcd.print("Seleccione un");
+    lcd.setCursor(0,1);
+    lcd.print("Jugo");
+    if (key != NO_KEY){ 
       lcd.clear();
       lcd.setCursor(0,0);
       if(key == '1'){
         lcd.clear();
         lcd.setCursor(0,0);
         lcd.print("Eligio manzana");
-        digitalWrite(relay1, HIGH);
+        delay(2000);
+        digitalWrite(led1, HIGH);
+        lcd.clear();
+        lcd.setCursor(0,0);  
+        lcd.print("Sirviendo...");
         delay(5000);
-        digitalWrite(relay1, LOW);
+        digitalWrite(led1, LOW);
+        lcd.clear();
+        lcd.setCursor(0,0);
+        lcd.print("Retire el vaso");
+        delay(3000);
         lcd.clear();
         } 
       if(key == '2'){
         lcd.clear();
         lcd.setCursor(0,0);  
         lcd.print("Eligio naranja");
-        digitalWrite(relay2, HIGH);
+        delay(2000);
+        digitalWrite(led2, HIGH);
+        lcd.clear();
+        lcd.setCursor(0,0);  
+        lcd.print("Sirviendo...");
         delay(5000);
-        digitalWrite(relay2, LOW);
+        digitalWrite(led2, LOW);
+        lcd.clear();
+        lcd.setCursor(0,0);
+        lcd.print("Retire el vaso");
+        delay(3000);
         lcd.clear();
         }
       if(key == '3'){
         lcd.clear();
         lcd.setCursor(0,0);
         lcd.print("Eligio pomelo");
-        digitalWrite(relay3, HIGH);
+        delay(2000);
+        digitalWrite(led3, HIGH);
+        lcd.clear();
+        lcd.setCursor(0,0);  
+        lcd.print("Sirviendo...");
         delay(5000);
-        digitalWrite(relay3, LOW);
+        digitalWrite(led3, LOW);
+        lcd.clear();
+        lcd.setCursor(0,0);
+        lcd.print("Retire el vaso");
+        delay(3000);
         lcd.clear();
         }
       if(key == 'A'){
         lcd.clear();
         lcd.setCursor(0,0);
         lcd.print("Eligio limon");
-        digitalWrite(relay4, HIGH);
+        delay(2000);
+        digitalWrite(led4, HIGH);
+        lcd.clear();
+        lcd.setCursor(0,0);  
+        lcd.print("Sirviendo...");
         delay(5000);
-        digitalWrite(relay4, LOW);
+        digitalWrite(led4, LOW);
         lcd.clear();
         }
+    }
        } //cierre si el vaso esta vacio
   } //cierre de loop
 
